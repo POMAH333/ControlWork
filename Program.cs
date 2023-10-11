2675ed4c355c602227ec708e0cdbad663ee70ae5
POMAH <pomka333@mail.ru>
2023-10-11 11:37:55 +0700

    Добавление меню

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+//-----------------------------------------------------------------------------------
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Выберите способ заполнения массива:");
+    Console.WriteLine("1) Ручное заполнение, посредством ввода строк с клавиатуры");
+    Console.WriteLine("2) Автоматическое заполнение из заданного массива строк");
+    Console.WriteLine();
+    Console.WriteLine("0) Или введите 0 для выхода из программы");
+    int menuNum = SetNumber("");
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
+
+    switch (menuNum)
+    {
+        case 0: return;
+        case 1: TaskStr(); break;
+        case 2: TaskStr(true); break;
+        default: Console.WriteLine($"Задачи №{menuNum}, не существует"); break;
+    }
+}
+
+//-----------------------------------------------------------------------------------
+
+int SetNumber(string messageEnt)
+{
+    Console.WriteLine(messageEnt);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+
+
+void TaskStr(bool autoArray = false)
+{
+    Console.WriteLine();
+
+    if (autoArray)
+    {
+        
+    }
+    else
+    {
+        
+    }
+}
+
+
